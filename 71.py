e1a3aa2fb9876ace3dc671346c2ef78297bf4bb7
MicaelSousa15 <micaels.barbosa@hotmail.com>
2017-07-20 18:17:45 -0300

    Update 71.py

--- 71.py
+++ 71.py
@@ -1,4 +1,8 @@
-idade1 = 18
-idade2 = 18
-print(idade1 and idade2 == 18)
-# O resultado vai ser True porque as 2 são igual a 18
+>>> idade1 = 17
+>>> idade2 = 18
+>>> idade >= 17 and idade2 >= 21
+True
+>>> # Retorna True porque ele comparou as 2 e as 2 deu positivo
+>>> idade >= 17 or idade2
+True
+>>> # Retorna True porque idade é maior que 17 ou 18
