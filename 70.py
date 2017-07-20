73ed89586998cc430a052d9bd0fef8acaec17efd
MicaelSousa15 <micaels.barbosa@hotmail.com>
2017-07-20 19:00:10 -0300

    Delete 70.py

--- 70.py
+++
@@ -1,8 +0,0 @@
->>> idade = 18
->>> idade2 = 21
->>> idade >= 24 and idade2
-False
->>> #Retorna falso porque ele compara idade a 24 e ao valor da variavel 2
->>> idade >= 17 or idade2
-True
->>> #Retorna verdadeiro porque ele compara uma das duas
