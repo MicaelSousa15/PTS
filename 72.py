9b8a293617de277fdda45eb28b9c0a501d1c7092
MicaelSousa15 <micaels.barbosa@hotmail.com>
2017-07-20 19:00:18 -0300

    Delete 72.py

--- 72.py
+++
@@ -1,7 +0,0 @@
->>> idade1 = 17
->>> idade2 = 18
->>> idade >= 17 or idade2 >= 21
-True
->>> idade >= 17 or idade2
-True
->>> # Os 2 deram True porque ele compara ou um ou outro, então se algum estiver certo, vai ser True
