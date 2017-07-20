ffdce01da3b034b4d3a3cce5d49770c68c6a54e5
MicaelSousa15 <micaels.barbosa@hotmail.com>
2017-07-20 19:00:14 -0300

    Delete 71.py

--- 71.py
+++
@@ -1,8 +0,0 @@
->>> idade1 = 17
->>> idade2 = 18
->>> idade >= 17 and idade2 >= 21
-True
->>> # Retorna True porque ele comparou as 2 e as 2 deu positivo
->>> idade >= 17 or idade2
-True
->>> # Retorna True porque idade é maior que 17 ou 18
