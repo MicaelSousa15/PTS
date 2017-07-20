7fe8dc74cb1c29c2abbacb3265df77531cd5591c
MicaelSousa15 <micaels.barbosa@hotmail.com>
2017-07-20 18:59:00 -0300

    Delete 05.py

--- 05.py
+++
@@ -1,7 +0,0 @@
-a = 2+3*8
-print(a)
-b = (2+3)*8
-print(b)
-# A variavel a, ele primeiro multiplicou e depois somos
-# A variavel b, ela primeirou somou e depois multiplicou
-# Toda operação que estiver dentro de parenteses vai ter prioridade
